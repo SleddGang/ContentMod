27ef661fa74d364f84088a20103fd08560172d97
alec.millar94@gmail.com <alec.millar94@gmail.com>
2020-12-21 14:37:19 -0500

    Added and cleaned the Rognir base code. Will still need to address existing bugs.

---
+++ Items/VikingCrown.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ContentMod.Items
+{
+    class VikingCrown : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Makes undead in the frozen biome friendly.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            item.value = Item.sellPrice(silver: 30);
+            item.rare = ItemRarityID.Expert;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<VoidPlayer>().vikingCrown = true;
+        }
+    }
+}
